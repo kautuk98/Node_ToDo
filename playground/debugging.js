ed529d6fff6f5a0a097bf1041f44cf22d68cc7aa
Kautuk <43730224+kautuk98@users.noreply.github.com>
2019-07-06 02:25:12 +0530

    Update debugging.js

--- playground/debugging.js
+++ playground/debugging.js
@@ -1,3 +1,6 @@
+// This is a Debugging example for a code to be debugged using ChromeDev Tools.
+// Through various commands you can come across the bugs in your code.
+ 
 var person = {
     name: "kautuk",
     age: 20
@@ -6,6 +9,7 @@ var person = {
 
 person.con = 123456789;
 
+// This debugger is added to stop the sequence here only.
 debugger; 
 person.s = "Male";
 
